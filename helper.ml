40604410c054049dedf4731640e20b9fc2c48d48
mattj26 <matthewjacobson26@gmail.com>
2017-03-25 20:17:45 -0400

    Crawl fixed

--- helper.ml
+++ helper.ml
@@ -8,8 +8,17 @@ let add_key_pairs (words : string list)
                   (url : WT.LinkSet.elt)
                   (dict : WT.LinkIndex.dict)
                 : WT.LinkIndex.dict =
-  let url_as_set = WT.LinkSet.singleton url in
-  List.fold_right (fun w d-> WT.LinkIndex.insert d w url_as_set) words dict;;
+  List.fold_right
+    (fun w d->
+      let links = WT.LinkIndex.lookup dict w in
+      match links with
+      | None -> WT.LinkIndex.insert d w (WT.LinkSet.singleton url)
+      | Some s ->
+          let newS = WT.LinkSet.insert s url in
+          WT.LinkIndex.insert d w newS) words dict;;
+
+
+
 
 
 
